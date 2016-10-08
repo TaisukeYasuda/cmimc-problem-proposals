7e2e983118bd91e663f19573bf5767000fec2cd3
Sam Li <zyzyxs@gmail.com>
2016-10-08 15:44:22 -0400

    Added passport features

--- index.js
+++ index.js
@@ -28,13 +28,51 @@ var connection = mysql.createConnection({
 
 connection.connect();
 
+passport.use(new LocalStrategy(
+  function(email, password, done) {
+    var userinfo = connection.query("SELECT staffid, email, password, type, name, andrewid"+
+      " FROM staff WHERE email='"+email+"' LIMIT 1");
+    if(userinfo.length == 0)
+    {//if the email is valid
+       return done(null, false, { message: 'Incorrect username.' });
+    }
+    else
+    {
+       var submitPassword = userinfo[2];
+       bcrypt.hash(password, 10, function(err, hash) {
+         if(hash === submitPassword)
+         {//if the password is correct
+           return done(null, userinfo);
+         }
+         else
+         {//if the password is not correct
+           return done(null, false, { message: 'Incorrect password.' });
+         }
+      });
+   }
+));
 
+app.configure(function() {
+  app.use(express.static('public'));
+  app.use(express.cookieParser());
+  app.use(express.bodyParser());
+  app.use(express.session({ secret: 'keyboard cat' }));
+  app.use(passport.initialize());
+  app.use(passport.session());
+  app.use(app.router);
+});
 // passport.use(new LocalStrategy(
 //   function(username, password, done) {
 //     User.findOne({ username: username}, function(err, user) {
 //       if (err) { return done(err); }
 //       if (!user)
-
+app.post('/login',
+  passport.authenticate('local'),
+  function(req, res) {
+    // If this function gets called, authentication was successful.
+    // `req.user` contains the authenticated user.
+    console.log("authentication success");
+  });
 
 http.createServer(function (request, response) {
    // Send the HTTP header
