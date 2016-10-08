6bfbcfff91ca6342b84f2f8181fac4ba46865799
Taisuke Yasuda <taisukeyasuda@Taisukes-MBP.wv.cc.cmu.edu>
2016-10-08 15:04:53 -0400

    add body-parser and cookie-parser

--- index.js
+++ index.js
@@ -6,8 +6,13 @@ var mysql = require("mysql");
 
 var mysql_password = process.env.MYSQL_PASSWORD;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
 var connection = mysql.createConnection({
-  host: "fdb7.biz.nf:3306", 
+  host: "fdb7.biz.nf:3306",
   user: "1991601_cmimc",
   password: mysql_password,
   database: "1991601_cmimc"
@@ -24,17 +29,13 @@ passport.use(new LocalStrategy(
 
 
 http.createServer(function (request, response) {
-   // Send the HTTP header 
+   // Send the HTTP header
    // HTTP Status: 200 : OK
    // Content Type: text/plain
    response.writeHead(200, {'Content-Type': 'html'});
-   
+
    // Send the response body as "Hello World"
-   if (something) {
-      response.end('<h1>hey</h1');
-   } else {
-      response.end('<h1>fuck u</h1>');
-   }
+   response.end('<h1>The CMIMC website is under construction!</h1>')
 }).listen(8081);
 // Console will print the message
 console.log('Server running at http://127.0.0.1:8081/');
