27db8c0c8813883561dd62d3c47cf0452fa84cdc
Cody Johnson <Cody@Codys-MBP.fios-router.home>
2016-10-09 00:21:06 -0400

    added controllers for each page

--- public/js/controller.js
+++ public/js/controller.js
@@ -1,8 +1,8 @@
-app.controller('mainCtrl', [
+app.controller('headerCtrl', [
 '$scope',
 function ($scope) {
   $scope.logged_in = true;
-  $scope.account_class = 'Admin';
+  $scope.account_class = 'Member';
   $scope.test = 'Hello world!';
 }]);
 
@@ -15,7 +15,53 @@ function ($scope) {
 app.controller('signupCtrl', [
 '$scope', '$http',
 function ($scope, $http) {
-  $scope.signup = function() {
+  // submit:
+  // INSERT INTO staff (email, password, type, name, andrewid, salt, jwt) VALUES ()
+}]);
 
-  }
+app.controller('proposeCtrl', [
+'$scope', '$http',
+function ($scope, $http) {
+  // submit:
+  // INSERT INTO proposals (staffid, topic, problem, answer, solution, difficulty) VALUES ()
+}]);
+
+app.controller('editProbCtrl', [
+'$scope', '$http',
+function ($scope, $http) {
+  // submit:
+  // UPDATE proposals SET topic=, difficulty=, problem=, answer=, solution= WHERE probid=
+  // delete:
+  // DELETE FROM proposals WHERE probid=
+}]);
+
+app.controller('loginCtrl', [
+'$scope', '$http',
+function ($scope, $http) {
+  // log in:
+  // SELECT * FROM staff WHERE email=
+}]);
+
+app.controller('manageContestCtrl', [
+'$scope', '$http',
+function ($scope, $http) {
+  // 
+}]);
+
+app.controller('probBankCtrl', [
+'$scope', '$http',
+function ($scope, $http) {
+  // 
+}]);
+
+app.controller('myProposalsCtrl', [
+'$scope', '$http',
+function ($scope, $http) {
+  // 
+}]);
+
+app.controller('viewProposalCtrl', [
+'$scope', '$http',
+function ($scope, $http) {
+  // 
 }]);
