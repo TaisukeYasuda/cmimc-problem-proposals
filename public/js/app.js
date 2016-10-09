e88b8ab1a8fbaf143695b73c8476f145f7119313
Taisuke Yasuda <taisukeyasuda@Taisukes-MBP.fios-router.home>
2016-10-08 21:28:34 -0400

    adding angular js to login

--- public/js/app.js
+++ public/js/app.js
@@ -1,18 +1,13 @@
 var app = angular.module('mainApp', ['ngRoute']);
 
-app.controller('mainCtrl', [
-'$scope',
-function ($scope) {
-  $scope.test = 'Hello world!';
-}]);
-
 app.config(function($routeProvider) {
   $routeProvider
   .when("/proposals", {
     templateUrl : "templates/proposals.html"
   })
   .when("/login", {
-    templateUrl : "templates/login.html"
+    templateUrl : "templates/login.html",
+    controller : "loginCtrl"
   })
   .when("/signup", {
     templateUrl : "templates/signup.html"
---
+++ public/js/controller.js
@@ -0,0 +1,11 @@
+app.controller('mainCtrl', [
+'$scope',
+function ($scope) {
+  $scope.test = 'Hello world!';
+}]);
+
+app.controller('loginCtrl', [
+'$scope',
+function ($scope) {
+  $scope.test = 'Hello world!';
+}]);
