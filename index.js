ecb7ee781558f60e9e407b18c515c44d4bc36cbf
Taisuke Yasuda <taisukeyasuda@Taisukes-MBP.fios-router.home>
2016-10-15 17:40:20 -0400

    listen on process.env.PORT

--- index.js
+++ index.js
@@ -73,6 +73,7 @@ app.use(passport.session());
 var routes = require('./routes');
 app.use('/', routes);
 
-var server = app.listen(3000, function () {
-   console.log("Server running at http://localhost:3000/")
+var server = app.listen(process.env.PORT || 3000, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
 });
