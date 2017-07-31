77127fc7c16240c66d4292231739ea6062d1fd55
Taisuke Yasuda <taisukey@andrew.cmu.edu>
2017-07-31 18:38:33 -0400

    factor out socket.io code

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+/*******************************************************************************
+ *
+ * Run server.
+ *
+ ******************************************************************************/
+
 const express = require('express'),
       app = express(),
       server = require('http').Server(app),
-      io = require('socket.io')(server),
       path = require('path'),
       favicon = require('serve-favicon'),
       bodyParser = require('body-parser'),
@@ -14,45 +19,25 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-/* home page */
+/* serve home page */
 app.get('/', function (req, res) {
    res.sendFile( __dirname + '/' + 'index.html' );
 })
 
-const connection = require('./config/database');
-
-/*******************************************************************************
- *
- * Run server. 
- *
- ******************************************************************************/
-
-const routes = require('./routes')(connection),
+/* start database connection and route endpoints */
+const connection = require('./config/database'),
+      routes = require('./routes')(connection),
       staffRoutes = require('./routes/staff')(connection),
       subjectsRoutes = require('./routes/subjects')(connection);
 app.use('/', routes);
 app.use('/staff', staffRoutes);
 app.use('/subjects', subjectsRoutes);
 
+/* start http server */
 server.listen(process.env.PORT || 8000, function () {
   var port = server.address().port;
   console.log('CMIMC problem proposals running on port', port);
 });
 
-/*******************************************************************************
- *
- * Socket IO server. 
- *
- ******************************************************************************/
-
-io.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-
-  socket.on('my other event', function(data) {
-    console.log(data);
-  });
-
-  socket.on('ailee', function(data) {
-    console.log(data);
-  });
-});
+/* start socket.io server */
+require('./config/socket')(server);
