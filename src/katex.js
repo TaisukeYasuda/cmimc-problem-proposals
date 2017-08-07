32c4ea47303b0d774620e51391848bffcb09420f
Taisuke Yasuda <taisukey@andrew.cmu.edu>
2017-08-06 21:53:52 -0400

    fix katex rendering issue when element is not provided

--- src/katex.js
+++ src/katex.js
@@ -1,10 +1,12 @@
 export default elem => {
+  if (!elem) return;
   renderMathInElement(elem, {
     delimiters: [
       {left: '$$', right: '$$', display: true},
       {left: '\\[', right: '\\]', display: true},
       {left: '\\(', right: '\\)', display: false},
       {left: '$', right: '$', display: false}
-    ]
+    ],
+    throwOnError: false
   });
 };
