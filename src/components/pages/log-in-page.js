7624dc728e3ab3b0ca61745d1f26dc8a396fa048
Cody Johnson <ctj@math.cmu.edu>
2017-08-20 14:48:05 -0400

    better login page

--- src/components/pages/log-in-page.js
+++ src/components/pages/log-in-page.js
@@ -11,16 +11,10 @@ import Error from "../error";
 const LoginPage = ({ authError, authMessage }) => (
   <Row className="container">
     <Error s={12} error={ authError } message={ authMessage } />
-    <Col m={5} s={12} style={{marginTop: "24px"}}>
+    <Col l={6} s={12} offset={"l3"} style={{marginTop: "24px"}}>
       <Card>
         <h2 className="teal-text text-darken-4">Log In</h2>
         <LoginForm />
-      </Card>
-    </Col>
-    <Col m={5} s={12} offset="m1" style={{marginTop: "24px"}}>
-      <Card>
-        <h2 className="teal-text text-darken-4">Sign Up</h2>
-        <SignupForm />
       </Card>
     </Col>
   </Row>
