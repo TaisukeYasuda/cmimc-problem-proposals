21c73405d6c132c0a36e49b5f4905ff9cdec7a38
Cody Johnson <ctj@math.cmu.edu>
2017-08-22 18:40:43 -0400

    started test solve

---
+++ src/components/pages/test-solve-page.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Row, Col } from "react-materialize";
+import {  } from "../utilities";
+
+const TestSolvePage = ({ message }) => (
+  <Row className="container">
+    sup
+  </Row>
+);
+
+export default TestSolvePage;
