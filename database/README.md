0e39a1da5b8fdb1ea9bb1e91be64870d5b1ebc6e
Taisuke Yasuda <taisukey@andrew.cmu.edu>
2017-08-22 13:41:44 -0400

    add short name to competitions

--- database/README.md
+++ database/README.md
@@ -22,6 +22,7 @@
 ```
 { 
   name: String,
+  short_name: String,
   website: String,
   location: String,
   contests: [ Contest ],
--- database/competition.js
+++ database/competition.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose'),
 
 const competitionSchema = new Schema({
   name: { type: String, required: true, unique: true },
+  short_name: { type: String, required: true },
   website: String,
   location: { type: String, required: true },
   active_contests: [ { type: Schema.Types.ObjectId, ref: 'Contest' } ],
@@ -14,7 +15,11 @@ const competitionSchema = new Schema({
   updated: { type: Date, required: true },
 });
 
-competitionSchema.pre('save', next => {
+competitionSchema.pre('save', function(name) {
+  if (!this.short_name) {
+    this.short_name = this.name;
+  }
+
   const now = new Date();
   if (!this.created) this.created = now;
   if (!this.updated) this.updated = now;
