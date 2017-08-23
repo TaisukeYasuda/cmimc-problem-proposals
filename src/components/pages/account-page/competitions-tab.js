4080a143cb16501b0b3c0ee6f5b50c4bb0f3111d
Taisuke Yasuda <taisukey@andrew.cmu.edu>
2017-08-23 13:21:56 -0400

    completely factored account page

---
+++ src/components/pages/account-page/competitions-tab.js
@@ -0,0 +1,73 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { Row, Col, Table, Button, Modal, Input } from "react-materialize";
+
+import { RightButtonPanel } from "../../utilities";
+import CreateCompetitionForm from "../../forms/create-competition";
+
+const CompetitionsTable = ({ competitions }) => {
+  const statusOptions = {
+    "Member": <div><li><a href="#" className="teal-text text-darken-3">Leave competition</a></li></div>,
+    "Director": (
+      <div>
+        <li><a href="#" className="teal-text text-darken-3">Leave competition</a></li>
+        <li><a href="#" className="teal-text text-darken-3">Add new members</a></li>
+        <li><a href="#" className="teal-text text-darken-3">Add new directors</a></li>
+        <li><a href="#" className="teal-text text-darken-3">Step down as director</a></li>
+      </div>
+    ),
+    "Pending": <div><li><a href="#" className="teal-text text-darken-3">Cancel request</a></li></div>
+  };
+
+  return (
+    <Table>
+      <thead>
+        <tr>
+          <th>Competition</th>
+          <th>Membership Status</th>
+          <th>Options</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {
+          competitions.map((competition, key) =>
+            <tr key={key}>
+              <td>{competition.name}</td>
+              <td>{competition.membershipStatus}</td>
+              <td>
+                <ul>
+                  { statusOptions[competition.membershipStatus] }
+                </ul>
+              </td>
+            </tr>
+          )
+        }
+      </tbody>
+    </Table>
+  );
+};
+
+const CompetitionsTab = ({ competitions }) => (
+  <Col s={12}>
+    <Row>
+      <CompetitionsTable competitions={ competitions } />
+      <RightButtonPanel>
+        <form>
+          <Modal header="Join a Competition" trigger={<Button className="teal darken-3">Join a Competition</Button>} actions={<div>
+            <Button flat modal="close" waves="light">Cancel</Button>
+            <Button flat modal="close" waves="light">Join</Button>
+            </div>}>
+
+            <Input label="Search competitions" />
+            Your join request will be reviewed by the directors of (CMIMC).
+          </Modal>
+        </form><br />
+        <CreateCompetitionForm />
+      </RightButtonPanel>
+    </Row>
+  </Col>
+);
+
+export default CompetitionsTab;
